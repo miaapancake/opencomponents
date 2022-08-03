da21372e7cf0633f1cb53216b174ae530a986890
AlternativeCorn <miaalternativecorn@gmail.com>
2022-08-03 14:52:27 +0200

    Improved css build tools

--- tools/compilesass.js
+++ tools/compilesass.js
@@ -1,8 +1,15 @@
 const sass = require('sass');
-const {readdir, writeFile} = require('fs/promises');
+const {readdir, writeFile, stat} = require('fs/promises');
 const { resolve } = require('path');
 let postcss = require('postcss')
 let autoprefixer = require('autoprefixer');
+let {cyanBright, bold, green} = require('chalk');
+
+
+let [bin, scriptPath, srcPath, outputPath] = process.argv;
+
+
+if(!outputPath) throw "Please specify an output path";
 
 async function getFiles(dir) {
     const dirents = await readdir(dir, { withFileTypes: true });
@@ -15,17 +22,24 @@ async function getFiles(dir) {
 
 (async () => {
 
+    console.log(cyanBright(`CSS`) + bold(' Compiling directory: ') + srcPath);
+
     let output = [];
 
-    for(let file of await getFiles('./src')) {
+    for(let file of await getFiles(srcPath)) {
         if(file.endsWith('.scss')) {
+            console.log(cyanBright(`CSS`) + bold(' Compiling file: ') + file);
             const css = sass.compile(file, {style: 'compressed'}).css;
             output.push(css);
         }
     }
 
-    const result = await postcss(autoprefixer()).process(output.join(''));
+    const result = await postcss(autoprefixer()).process(output.join(''), {from: undefined});
+
     
-    await writeFile('./lib/style.css',result.css);
+    await writeFile(outputPath, result.css);
+    const {size} = await stat(outputPath);
+
+    console.log(cyanBright(`CSS `) + bold(outputPath) + green(` ${(size / (1024)).toFixed(2)} KiB`));
 
 })();
