91718397b866a7422fb24fe3b0bdb4de3c479648
miaapancake <mia@openthingies.com>
2022-10-11 13:03:44 +0200

    Corrected the url in the storybook theme.js

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'light',
   brandTitle: 'OpenComponents',
-  brandUrl: 'https://components.openthingies.com',
+  brandUrl: 'https://docs.openthingies.com/components',
   brandImage: 'https://git.openthingies.com/OpenThingies/assets/raw/branch/mistress/img/OpenComponents/opencomponentsfull.png',
   brandTarget: '_self'
 });
